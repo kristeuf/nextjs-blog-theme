ba6374f243682bef2eb586fd58a2867f192ef5a9
kristouf <51321892+kristeuf@users.noreply.github.com>
2022-07-16 16:26:04 +0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Index({ posts, globalData }) {
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
-        <ul className="w-full">
+        <ul className="w-full rounded-lg">
           {posts.map((post) => (
             <li
               key={post.filePath}
